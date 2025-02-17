ecac10393352ca718aef2f2b6872a1b82f43a055
rahul <devararahul56@gmail.com>
2025-02-17 11:16:47 -0600

    array and loops excercises

---
+++ script/11x.js
@@ -0,0 +1,58 @@
+const TodoList = [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];
+
+renderTodoList();
+
+function renderTodoList(){
+
+  let TodoListHTML = '';
+
+for (let i=0; i < TodoList.length; i++) {
+  const TodoObject = TodoList[i];
+  //const name = TodoObject.name;
+  //const dueDate = TodoObject.dueDate;
+  const {name, dueDate} = TodoObject;
+  const html= `
+  <div class=""> ${name}</div> 
+  <div>${dueDate}</div>
+  <button class="delete-todo-button" onclick="
+    TodoList.splice(${i},1);
+    renderTodoList();
+  ">Delete</button>
+  `;
+  TodoListHTML += html;
+};
+
+
+
+document.querySelector('.js-todo-list')
+ .innerHTML = TodoListHTML;
+};
+
+function addTodo () {
+ const inputElement = document.querySelector('.js-name-input');
+ const name = inputElement.value;
+
+ const dateInputElement = document.querySelector('.js-due-date-input')
+ const dueDate = dateInputElement.value;
+
+ TodoList.push({
+ // name: name,
+ // dueDate: dueDate,
+  name,
+  dueDate
+ }
+);
+
+
+
+ inputElement.value = '';
+
+ renderTodoList();
+
+};
